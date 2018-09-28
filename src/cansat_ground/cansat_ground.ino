68c9f9894d9c9c6fd1c652e92d8fe12b0bd1997c
Stavros Fotoglou <38467107+stav98@users.noreply.github.com>
2018-09-28 13:38:34 +0300

    Add files via upload

---
+++ src/cansat_ground/cansat_ground.ino
@@ -0,0 +1,114 @@
+#include "Wire.h"
+#include <SPI.h>
+#include <LowPower.h>
+#define RADIO_LED   9
+
+#include "rfm69.h"
+
+char tx_buffer[61];
+byte buf_cnt = 0;
+uint32_t rx_time;
+long int pkt_count;
+boolean cmdtx_flag = false, blink_ack = false;
+String sat_data, data1, data2;
+int idx1;
+word pkt_cnt, pkt_cnt_old = 65535;
+
+void setup() 
+{
+ pinMode(RADIO_LED, OUTPUT);
+ Serial.begin(115200);
+ Serial.print(F("Program Started\n"));
+ delay(10);
+ radio_init();
+}
+
+void loop() 
+{
+ //Έλεγξε αν υπάρχει ολοκληρωμένο πακέτο προς επεξεργασία
+ if (radio.receiveDone())
+    {
+     rx_time = millis();
+     sat_data = String((char*)radio.DATA);
+     idx1 = sat_data.indexOf(';');
+     data1 = sat_data.substring(0, idx1);
+     pkt_cnt = data1.toInt();
+     //Serial.print((char*)radio.DATA); //Εμφανίζει την απάντηση από τον δορυφόρο
+     if (pkt_cnt != pkt_cnt_old)
+        {
+         Serial.print(sat_data);
+         pkt_cnt_old = pkt_cnt;
+        }
+     else
+         Serial.println("---------------------- Duplicate ----------------");
+         
+     //Serial.println(pkt_cnt);
+     Serial.print(F("L>RSSI: "));
+     Serial.print(radio.RSSI);
+     Serial.print(" #");
+     Serial.println(pkt_count); //Debug
+     //Αν έλαβε πακέτο Ping Request τότε γύρισέ το πίσω στον δορυφόρο
+     //if (strcmp_P(radio.DATA, PSTR("PngRq:0123456789abcdefghijklmnopqrsruvwxyz!@#$%^&*-+.ABCDEFG")) == 0)
+     if (strlen((char*)radio.DATA) == 60 && radio.DATA[0]=='P' && radio.DATA[1]=='n' && radio.DATA[2]=='g')
+        {
+         //Serial.println("OK match"); //Debug
+         radio.sendWithRetry(SENDER, radio.DATA, strlen(radio.DATA), 1, 20);
+        }
+     pkt_count++;
+     //Έλεγξε μήπως ο δορυφόρος ζήτησε επιβεβαίωση λήψης (ACK)
+     if (radio.ACKRequested())
+         radio.sendACK();
+    }
+ //LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_ON); //sleep Moteino in low power mode (to save battery)
+ //Αν υπάρχουν δεδομένα στη σειριακή
+ if (Serial.available())
+    {
+     char c = Serial.read();
+     if (c != '\r')
+         tx_buffer[buf_cnt++] = c;
+     else
+        {
+         tx_buffer[buf_cnt] = '\0';
+         buf_cnt = 0;
+         cmdtx_flag = true;
+        }
+    }
+ if (cmdtx_flag)
+    {
+     if (millis() - rx_time > 2000) rx_time = millis();
+     if ((millis() - rx_time > 25) && (millis() - rx_time < 75)) //100-150
+        {
+         if (radio.sendWithRetry(SENDER, tx_buffer, strlen(tx_buffer), 1, 20)) //3 Αν το έστειλε και πήρε απάντηση (προσπαθεί 3 φορές), τότε να εμφανίσει μήνυμα για debug. 
+            {
+             Serial.println(F("L>Command send and verified")); 
+             cmdtx_flag = false;
+             blink_ack = true;
+            }
+         else
+             Serial.println(F("L>Command not acknoweleged"));
+         //cmdtx_flag = false;
+        }
+    }
+ Blink(RADIO_LED);
+}
+
+void Blink(byte PIN)
+{ 
+ static uint32_t ms_now;
+ if (blink_ack)
+    {
+     if (!digitalRead(PIN))
+        {
+         ms_now = millis();
+         digitalWrite(PIN, HIGH);
+        }
+     else
+        {
+         if (millis() - ms_now > 200)
+            {
+             digitalWrite(PIN, LOW);
+             blink_ack = false;
+            }
+        }
+    }
+}
---
+++ src/cansat_ground/rfm69.h
@@ -0,0 +1,50 @@
+#include <RFM69.h>
+//#include <RFM69_ATC.h>    
+
+void radio_init(void);
+
+#define NETWORKID     100  //Ίδιο για όλους του κόμβους
+#define RECEIVER      1
+#define SENDER        2
+#define NODEID        RECEIVER  //Ο σταθμός εδάφους είναι δέκτης
+#define FREQUENCY     RF69_433MHZ   //Μπάντα 433MHz
+//#define ENCRYPTKEY    "ABCDEFGHIJKLMNOP" //16 χαρακτήρες, ίδιο για όλους τους κόμβους
+
+//*****************************************************************************************************************************
+//#define ENABLE_ATC      //comment out this line to disable AUTO TRANSMISSION CONTROL
+#define ATC_RSSI        -75
+//*********************************************************************************************
+
+#define LED           9 // Moteinos have LEDs on D9
+#define BUTTON_INT    1 //user button on interrupt 1 (D3)
+#define BUTTON_PIN    3 //user button on interrupt 1 (D3)
+
+#define LED_GREEN       4 //GREEN LED on the SENDER
+#define LED_RED         5 //RED LED on the SENDER
+#define RX_TOGGLE_PIN   7 //GPIO to toggle on the RECEIVER
+
+#ifdef ENABLE_ATC
+  RFM69_ATC radio;
+#else
+  RFM69 radio;
+#endif
+
+void radio_init(void)
+{
+  radio.initialize(FREQUENCY, NODEID, NETWORKID);
+  radio.setHighPower(true); //Αυτό μόνο αν το module είναι HW ή HCW δηλ. 100mW
+  radio.setPowerLevel(31);  //0-31 = 5-20dbm
+  //radio.encrypt(ENCRYPTKEY); 
+  #ifdef ENABLE_ATC
+     radio.enableAutoPower(ATC_RSSI);
+  #endif
+  radio.setFrequency(434000000); //για Ελλάδα 433.05 - 434.79 MHz
+  Serial.print("Listening at ");
+  Serial.println(radio.getFrequency());
+  Serial.flush();
+  //pinMode(BUTTON_PIN, INPUT_PULLUP);
+  //pinMode(LED, OUTPUT);
+  //attachInterrupt(BUTTON_INT, handleButton, FALLING);
+}
+
+
